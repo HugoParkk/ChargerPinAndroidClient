16a2eb74970774b873088a6a17e6fcb53bdb88cc
박종연 <20041230andy@gmail.com>
2022-07-12 20:08:38 +0900

    Added LoginActivities

---
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/LoginActivity.kt
@@ -0,0 +1,14 @@
+package xyx.pokoed.chargerpinandroidclient.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyx.pokoed.chargerpinandroidclient.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
