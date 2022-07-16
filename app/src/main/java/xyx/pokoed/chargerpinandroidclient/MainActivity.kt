02917eba4c26ae2d40bb51e20e7d6b9af3e3517d
박종연 <20041230andy@gmail.com>
2022-07-16 15:46:37 +0900

    Update MainActivity.kt

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/MainActivity.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/MainActivity.kt
@@ -1,11 +1,24 @@
 package xyx.pokoed.chargerpinandroidclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import xyx.pokoed.chargerpinandroidclient.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var loginBtn: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn = findViewById(R.id.btn_login)
+
+
+        loginBtn.setOnClickListener {
+            val loginIntent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
     }
 }
