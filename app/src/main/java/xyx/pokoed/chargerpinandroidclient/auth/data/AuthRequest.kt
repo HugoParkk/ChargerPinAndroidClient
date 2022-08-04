7d2e400069057ab23523fe0faa1df00b78d29657
박종연 <20041230andy@gmail.com>
2022-08-04 21:14:47 +0900

    Added DTO

---
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/AuthRequest.kt
@@ -0,0 +1,6 @@
+package xyx.pokoed.chargerpinandroidclient.auth.data
+
+data class AuthRequest(
+    val userId: String,
+    val userPassword: String
+)
---
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/AuthResponse.kt
@@ -0,0 +1,8 @@
+package xyx.pokoed.chargerpinandroidclient.auth.data
+
+import com.google.gson.annotations.SerializedName
+
+data class AuthResponse (
+    val code: Int,
+    val msg: String
+)
