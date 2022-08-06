604df8b675117932540aa1e6c8121277d3f9bafe
박종연 <20041230andy@gmail.com>
2022-08-07 03:29:35 +0900

    Update RetrofitService.kt

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/retrofit/RetrofitService.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/retrofit/RetrofitService.kt
@@ -2,10 +2,10 @@ package xyx.pokoed.chargerpinandroidclient.retrofit
 
 import retrofit2.Call
 import retrofit2.http.*
-import xyx.pokoed.chargerpinandroidclient.auth.data.AuthRequest
-import xyx.pokoed.chargerpinandroidclient.auth.data.AuthResponse
+import xyx.pokoed.chargerpinandroidclient.auth.data.LoginRequest
+import xyx.pokoed.chargerpinandroidclient.auth.data.LoginResponse
 
 interface RetrofitService {
     @POST("auth/login")
-    fun login(@Body user: AuthRequest): Call<AuthResponse>
+    fun login(@Body user: LoginRequest): Call<LoginResponse>
 }
