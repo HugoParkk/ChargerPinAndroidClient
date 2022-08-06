5572504569968166630fc8404fa244c87672eb62
박종연 <20041230andy@gmail.com>
2022-08-07 03:29:21 +0900

    Auth DTO name changed Login

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/LoginRequest.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/LoginRequest.kt
@@ -1,6 +1,6 @@
 package xyx.pokoed.chargerpinandroidclient.auth.data
 
-data class AuthRequest(
+data class LoginRequest(
     val userId: String,
     val userPassword: String
 )
--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/LoginResponse.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/data/LoginResponse.kt
@@ -2,7 +2,7 @@ package xyx.pokoed.chargerpinandroidclient.auth.data
 
 import com.google.gson.annotations.SerializedName
 
-data class AuthResponse (
+data class LoginResponse (
     val code: Int,
     val msg: String
 )
