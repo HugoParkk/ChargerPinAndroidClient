6a9025739823ba4feeb36228429bd3fefc82b632
박종연 <20041230andy@gmail.com>
2022-08-11 00:09:13 +0900

    Create ChagerListActivity.kt

---
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/chagerList/ChagerListActivity.kt
@@ -0,0 +1,13 @@
+package xyx.pokoed.chargerpinandroidclient.chagerList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyx.pokoed.chargerpinandroidclient.R
+
+class ChagerListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chager_list)
+
+    }
+}
