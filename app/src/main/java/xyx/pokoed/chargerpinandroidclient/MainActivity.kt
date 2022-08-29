8e9661f2eb9a1f5c7f65a407e8c21c9f99903449
박종연 <20041230andy@gmail.com>
2022-08-30 02:46:12 +0900

    Update MainActivity.kt

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/MainActivity.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/MainActivity.kt
@@ -3,38 +3,61 @@ package xyx.pokoed.chargerpinandroidclient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import xyx.pokoed.chargerpinandroidclient.auth.LoginActivity
-import xyx.pokoed.chargerpinandroidclient.auth.RegisterActivity
-import xyx.pokoed.chargerpinandroidclient.chagerList.ChagerListActivity
+import xyx.pokoed.chargerpinandroidclient.auth.data.LoginRequest
+import xyx.pokoed.chargerpinandroidclient.auth.data.LoginResponse
+import xyx.pokoed.chargerpinandroidclient.retrofit.RetrofitClient
 
 class MainActivity : AppCompatActivity() {
-    lateinit var toLoginBtn: Button;
-    lateinit var toRegisterBtn: Button;
-    lateinit var toChargerListBtn: Button;
+    lateinit var homeLayout: LinearLayout
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toLoginBtn = findViewById(R.id.btn_to_login)
-        toRegisterBtn = findViewById(R.id.btn_to_register)
-        toChargerListBtn = findViewById(R.id.btn_to_charger_list)
+        supportActionBar?.hide()
 
-
-        toLoginBtn.setOnClickListener {
-            val loginIntent = Intent(this, LoginActivity::class.java)
+        if (App.isLogined) {
+            init()
+            settingListener()
+        } else {
+            val loginIntent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(loginIntent)
         }
-        toRegisterBtn.setOnClickListener {
-            val registerIntent = Intent(this, RegisterActivity::class.java)
-            startActivity(registerIntent)
-        }
-        toChargerListBtn.setOnClickListener {
-            val chagerListIntent = Intent(this, ChagerListActivity::class.java)
-            startActivity(chagerListIntent)
+
+    }
+
+    fun init() {
+        homeLayout = findViewById(R.id.home_ly)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+    }
+
+    fun settingListener() {
+        bottomNavigationView.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.tab_home -> replaceFragment(HomeFragment())
+                    R.id.tab_profile -> replaceFragment(ProfileFragment())
+                    R.id.tab_setting -> replaceFragment(SettingFragment())
+                }
+                true
+            }
+            selectedItemId = R.id.tab_home
         }
 
+    }
 
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(homeLayout.id, fragment).commit()
     }
 }
