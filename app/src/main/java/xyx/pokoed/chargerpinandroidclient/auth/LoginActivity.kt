e01f69046bfb04945d91f3cdaf012c14ed13110f
박종연 <20041230andy@gmail.com>
2022-08-30 02:45:23 +0900

    Update LoginActivity.kt

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/LoginActivity.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/auth/LoginActivity.kt
@@ -4,24 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import xyx.pokoed.chargerpinandroidclient.App
+import xyx.pokoed.chargerpinandroidclient.MainActivity
 import xyx.pokoed.chargerpinandroidclient.R
 import xyx.pokoed.chargerpinandroidclient.auth.data.LoginRequest
 import xyx.pokoed.chargerpinandroidclient.auth.data.LoginResponse
 import xyx.pokoed.chargerpinandroidclient.retrofit.RetrofitClient
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var inputId: EditText
-    lateinit var inputPwd: EditText
-    lateinit var btnLogin: Button
-    lateinit var toRegister: TextView
+    private lateinit var inputId: EditText
+    private lateinit var inputPwd: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var toRegister: TextView
+    private lateinit var autoLoginChk: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +31,43 @@ class LoginActivity : AppCompatActivity() {
         inputPwd = findViewById(R.id.input_pwd)
         btnLogin = findViewById(R.id.btn_login)
         toRegister = findViewById(R.id.to_register)
+        autoLoginChk = findViewById(R.id.checkAutoLogin)
+
+        val pref = getSharedPreferences("userInfo", MODE_PRIVATE)
+        val id = pref.getString("userId", "")
+        val pwd = pref.getString("userPassword", "")
+        val callLogin = RetrofitClient.api.login(LoginRequest(id!!, pwd!!))
+        Log.d("saasdf", id)
+        Log.d("saasdf", pwd)
+        callLogin.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(
+                call: Call<LoginResponse>,
+                response: Response<LoginResponse>
+            ) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(
+                        applicationContext,
+                        "${response.body()!!.user!!.userName} 입니다",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    val mainIntent = Intent(applicationContext, MainActivity::class.java)
+                    App.isLogined = true
+                    App.userName = response.body()!!.user!!.userId
+
+                    startActivity(mainIntent)
+                    overridePendingTransition(0, 0)
+
+                } else {
+                    Toast.makeText(applicationContext, "실패 400", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, "${t.message}", Toast.LENGTH_SHORT).show()
+                Log.d("asdf", t.message.toString())
+            }
+        })
 
         btnLogin.setOnClickListener {
             val id = inputId.text.toString()
@@ -44,9 +80,23 @@ class LoginActivity : AppCompatActivity() {
                     response: Response<LoginResponse>
                 ) {
                     if (response.isSuccessful()) {
-                        Toast.makeText(applicationContext, "${response.body()!!.user!!.userName} 입니다", Toast.LENGTH_SHORT).show()
-                        // id pwd 자동 로그인 때문에 저장
-                        App.instance.saveUserInfoData(id, pwd)
+                        Toast.makeText(
+                            applicationContext,
+                            "${response.body()!!.user!!.userName} 입니다",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        if (autoLoginChk.isChecked) {
+                            // id pwd 자동 로그인 때문에 저장
+                            App.instance.saveUserInfoData(id, pwd)
+                        }
+
+                        val mainIntent = Intent(applicationContext, MainActivity::class.java)
+                        App.isLogined = true
+                        App.userName = response.body()!!.user!!.userId
+
+                        startActivity(mainIntent)
+                        overridePendingTransition(0, 0)
                     } else {
                         Toast.makeText(applicationContext, "실패 400", Toast.LENGTH_SHORT).show()
                     }
