c0e8a62f5b1acac1b645a669e7d79398af7227ae
박종연 <20041230andy@gmail.com>
2022-08-30 02:46:08 +0900

    Update RetrofitService.kt

--- app/src/main/java/xyx/pokoed/chargerpinandroidclient/retrofit/RetrofitService.kt
+++ app/src/main/java/xyx/pokoed/chargerpinandroidclient/retrofit/RetrofitService.kt
@@ -2,12 +2,12 @@ package xyx.pokoed.chargerpinandroidclient.retrofit
 
 import retrofit2.Call
 import retrofit2.http.*
+import retrofit2.http.Body
 import xyx.pokoed.chargerpinandroidclient.auth.data.LoginRequest
 import xyx.pokoed.chargerpinandroidclient.auth.data.LoginResponse
 import xyx.pokoed.chargerpinandroidclient.auth.data.RegisterRequest
 import xyx.pokoed.chargerpinandroidclient.auth.data.RegisterResponse
-import xyx.pokoed.chargerpinandroidclient.chagerList.data.ChargerInfoDTO
-import xyx.pokoed.chargerpinandroidclient.chagerList.data.ChargerRequest
+import xyx.pokoed.chargerpinandroidclient.chagerList.data.*
 
 interface RetrofitService {
     @POST("auth/login")
@@ -18,4 +18,16 @@ interface RetrofitService {
 
     @POST("charger/info")
     fun search(@Body keyword: ChargerRequest): Call<ChargerInfoDTO>
+
+    @POST("charger/info")
+    fun search1(@Body keyword: ChargerRequest): Call<ChargerInfoDTO1>
+
+    @GET("bookmark/read/{userId}")
+    fun getBookmark(@Path("userId") userId: String): Call<Array<BookmarkDTO>>
+
+    @POST("bookmark/add")
+    fun addBookmark(@Body bookmarkDTO: BookmarkDTO): Call<BookmarkResponse>
+
+    @DELETE("bookmark/delete/{bookmarkId}/{userId}")
+    fun deleteBookmark(@Path("bookmarkId") bookmarkId: Int, @Path("userId") userId: String): Call<BookmarkResponse>
 }
